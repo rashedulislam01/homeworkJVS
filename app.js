bdba491719f6400b5b35b92465f66a7b06ccacdb
Rashedul Islam <67190791+rashedulislam01@users.noreply.github.com>
2023-02-22 13:04:27 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,139 @@
+// 1.variable name can not be any keywords
+// 2.variable name has to be in one word.No space 
+// 3.variable name can not have quotation 
+// 4.variable name can not start with a number but can ends with a number 
+/**
+// keywords
+let x;
+console.log(x);
+
+//undefined
+let name = "Rashed";
+console.log("Rashed");
+console.log("Rashed");
+console.log("Rashed");
+console.log("Rashed");
+console.log("Rashed");
+
+//step 2 Assigning valum
+// 1. = assingment operator
+// 2. value;
+let name;
+console.log(name);
+name = 'Rashed';
+console.log(name);
+
+
+let name = "Rashed";
+console.log(name);
+
+//naming convention
+let num=5;
+let numbertoAdd=5;
+
+//comelCase
+
+/**
+ * 1. camelCase=number To Add
+ * 2. sneke_case=Number_to-add
+ * 3.kebab-case=number-to-add
+ */
+
+let function=5;
+console.log(name);
+
+// Data types
+let num=2;
+
+//1. Reference type
+//2. primitive type
+//dynamically typed 
+let num=2;
+num='Rashed';
+console.log(num);
+
+//1.number=floating number
+let num=2;
+let isloading=false;
+
+console.log(num);
+
+// Numaric
+let price = 5;
+console.log(price);
+
+// String 
+let person ="Rashed";
+console.log(person);
+
+//type of operator
+let num = 2;
+let isloading = '2';
+
+console.log(typeof num);
+
+// Boolean =true/false
+let num
+let isloading=true;
+
+console.log(num);
+console.log(typeof  num);
+
+//undefined
+let num = null;
+console.log(typeof num);
+
+// addition
+let totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+
+// subtraction
+let priceDifferance = onionPrice - eggPrice;
+console.log(priceDifferance);
+
+// Multiplication
+let totalCost = onionPrice * eggPrice;
+console.log(totalCost);
+
+// division
+let money  = 10;
+let player = 5;
+let eachPlayer = money / player
+console.log(eachPlayer);
+
+//var, let, const
+//1. var, let = mulable variable
+//2. const = inntable variable
+
+let a = 2;
+console.log(a++);
+console.log(a);
+
+//assigment operators
+let a = 2;
+a = a + 2;
+console.log(a);
+
+let a = 5;
+let b = 6;
+
+let x = 10;
+let y = 11;
+
+console.log((a=b));
+console.log(a);
+
+let a = 5;
+let b = 6;
+console.log(a > b);
+
+// logical operators
+// && both true = true
+// && both false = false
+let a = 5;
+let b = 6;
+
+let c = 10;
+let d = 11;
+
+console.log(a + b > 0 && c+d > 0);
